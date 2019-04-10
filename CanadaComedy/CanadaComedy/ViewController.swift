c7b5266dfdb046a3ac23c796c4504292a943bae0
vikingv072 <viken041@gmail.com>
2019-04-10 15:03:38 -0400

    Integrated SwiftLint with Project

---
+++ CanadaComedy/CanadaComedy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CanadaComedy
+//
+//  Created by C02PX1DFFVH5 on 4/10/19.
+//  Copyright © 2019 C02PX1DFFVH5. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CanadaComedy/Pods/Target Support Files/Pods-CanadaComedyTests/Pods-CanadaComedyTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CanadaComedyTests : NSObject
+@end
+@implementation PodsDummy_Pods_CanadaComedyTests
+@end
