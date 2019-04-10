2ea730922f4a85a975748f49fcd34efa5c5d8d97
vikingv072 <viken041@gmail.com>
2019-04-10 16:11:30 -0400

    Added Network Service

---
+++ CanadaComedy/CanadaComedy/NetworkService.swift
@@ -0,0 +1,27 @@
+//
+//  NetworkService.swift
+//  CanadaComedy
+//
+//  Created by C02PX1DFFVH5 on 4/10/19.
+//  Copyright © 2019 C02PX1DFFVH5. All rights reserved.
+//
+
+import Foundation
+
+final class NetworkService {
+    static let shared = NetworkService()
+    let someUrl = URL(string: "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json")
+    func downloader(_ completion: @escaping (([CanadaModel]) -> Void)) {
+        guard let useUrl = someUrl else { return }
+        URLSession.shared.dataTask(with: useUrl){ (data, _ , _ ) in guard let useData = data else {
+                print("Going Wonky")
+                return}
+            print("Downloaded")
+            let decoder = JSONDecoder()
+            do {
+                let val = try? decoder.decode([CanadaModel].self, from: useData)
+                completion(val!)
+            } catch { print(error) }
+        }.resume()
+    }
+}
